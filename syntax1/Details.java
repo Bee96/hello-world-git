aa64e2f7f034fe653776dc615dba1fc19a533f1d
Bee96 <belennicora96@outlook.com>
2022-12-06 20:07:07 +0100

    docs: Cambio de nombre y localizacion

--- syntax1/Details.java
+++ syntax1/Details.java
@@ -2,6 +2,6 @@ package syntax1;
 
 public class Details{
     public static void main (String[] args){
-        System.out.println("Java program with diff file name and class name");
+        System.out.println("Java program with same file name and class name");
     }
 }
