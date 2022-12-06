828031492d2f0da9621a269c287525256db723b1
Bee96 <belennicora96@outlook.com>
2022-12-06 20:18:42 +0100

    feat: Añadimos comentario

--- syntax1/Details.java
+++ syntax1/Details.java
@@ -2,6 +2,7 @@ package syntax1;
 
 public class Details{
     public static void main (String[] args){
-        System.out.println("Java program with same file name and class name");
+        //This is a single line comment
+        System.out.println("Hello world!!");
     }
 }
