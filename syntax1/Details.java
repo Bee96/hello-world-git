90a6980e9bd24661c2894be15030816a221d8226
Bee96 <belennicora96@outlook.com>
2022-12-06 20:20:26 +0100

    feat: Añadimos comentario multilinea

--- syntax1/Details.java
+++ syntax1/Details.java
@@ -3,6 +3,13 @@ package syntax1;
 public class Details{
     public static void main (String[] args){
         //This is a single line comment
+
+        /*This
+         * is
+         * a
+         * multiline
+         * comment
+         */
         System.out.println("Hello world!!");
     }
 }
