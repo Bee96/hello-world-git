10d6a7565c9e5dd8c20a75d101b9a15b5fd126b3
Bee96 <belennicora96@outlook.com>
2022-12-17 20:20:49 +0100

    not local variables

--- dataTypes/variableType.java
+++ dataTypes/variableType.java
@@ -1,15 +1,29 @@
 package dataTypes;
 
 public class variableType{
+    // public void localVariable() {
+    //     String name = "Belen";
+    //     int marks = 95;
+    //     System.out.println(name + " scored " + marks + "%.");
+    // }
+
+    // public static void main(String[] args) {
+    //     variableType vt = new variableType();
+    //     vt.localVariable();
+    // }
+
     public void localVariable() {
         String name = "Belen";
         int marks = 95;
+    }
+
+    public void notLocalVariable(){
         System.out.println(name + " scored " + marks + "%.");
     }
 
     public static void main(String[] args) {
         variableType vt = new variableType();
-        vt.localVariable();
+        vt.notlocalVariable();
     }
     
 }
