d557f316ef97ebb676d9408ec6e6609ddc8805cb
Bee96 <belennicora96@outlook.com>
2022-12-17 20:42:04 +0100

    Añadir comentarios identificando cada práctica

--- dataTypes/variableType.java
+++ dataTypes/variableType.java
@@ -1,6 +1,7 @@
 package dataTypes;
 
 public class variableType{
+    /*Práctica declaración de variables locales */
     // public void localVariable() {
     //     String name = "Belen";
     //     int marks = 95;
@@ -12,18 +13,20 @@ public class variableType{
     //     vt.localVariable();
     // }
 
-    public void localVariable() {
-        String name = "Belen";
-        int marks = 95;
-    }
 
-    public void notLocalVariable(){
-        System.out.println(name + " scored " + marks + "%.");
-    }
+    /*Práctica declaración de variables no locales */
+    // public void localVariable() {
+    //     String name = "Belen";
+    //     int marks = 95;
+    // }
 
-    public static void main(String[] args) {
-        variableType vt = new variableType();
-        vt.notlocalVariable();
-    }
+    // public void notLocalVariable(){
+    //     System.out.println(name + " scored " + marks + "%.");
+    // }
+
+    // public static void main(String[] args) {
+    //     variableType vt = new variableType();
+    //     vt.notlocalVariable();
+    // }
     
 }
