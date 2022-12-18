07da48b2f4549306cad3b72d8f84a73c5255c103
Bee96 <belennicora96@outlook.com>
2022-12-18 19:56:21 +0100

    Practica variables estáticas de la clase

--- dataTypes/variableType.java
+++ dataTypes/variableType.java
@@ -29,7 +29,7 @@ public class variableType{
     //     vt.notlocalVariable();
     // }
     
-    /*Práctica con variables no státicas que se pueden usar por todos los métodos de la clase */
+    /*Práctica con variables no státicas (variables de instancia) que se pueden usar por todos los métodos de la clase */
     // public String name = "Ben";
     // public int marks = 75;
 
@@ -41,4 +41,16 @@ public class variableType{
     //     variableType vt  = new variableType();
     //     vt.instanceVariable();
     // }
+
+    /*Práctica con variables státicas (similar a las variables de instancia) que se pueden usar por todos los métodos de la clase */
+    // public static String name;
+    // public static int marks;
+
+    // public static void main(String[] args) {
+    //     name = "Pepe";
+    //     marks = 55;
+    //     System.out.println(name + " scored " + marks + "%.");
+    // }
+
+
 }
