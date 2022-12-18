a121c704d6e47af856027e657c735d9e0ccc0203
Bee96 <belennicora96@outlook.com>
2022-12-18 19:45:17 +0100

    Practica variables no estáticas

--- dataTypes/variableType.java
+++ dataTypes/variableType.java
@@ -1,7 +1,7 @@
 package dataTypes;
 
 public class variableType{
-    /*Práctica declaración de variables locales */
+    /*Práctica declaración de variables locales estáticas que se pueden usar solo por el método en el que están*/
     // public void localVariable() {
     //     String name = "Belen";
     //     int marks = 95;
@@ -29,4 +29,16 @@ public class variableType{
     //     vt.notlocalVariable();
     // }
     
+    /*Práctica con variables no státicas que se pueden usar por todos los métodos de la clase */
+    // public String name = "Ben";
+    // public int marks = 75;
+
+    // public void instanceVariable() {
+    //     System.out.println(name + " scored " + marks + "%.");
+    // }
+
+    // public static void main(String[] args) {
+    //     variableType vt  = new variableType();
+    //     vt.instanceVariable();
+    // }
 }
